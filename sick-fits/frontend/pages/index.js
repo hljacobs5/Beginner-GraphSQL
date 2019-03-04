e7d618072bc240ea0e438fc170edded78a43dac7
Haley Jacobs <haleyljacobs@gmail.com>
2019-03-04 10:27:22 -0700

    Added sell.js and adjusted index.js

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-
-class Home extends React.Component {
-	render() {
-		return <p>Hey!</p>;
-	}
-}
+import Link from 'next/link';
 
 const Home = props => (
 	<div>
 		<p>Hey!</p>
+		<Link href='/sell'>
+			<a>Sell!</a>
+		</Link>
 	</div>
 );
 
