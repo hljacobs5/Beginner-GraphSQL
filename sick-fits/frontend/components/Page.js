acb6d8c76cfd6dbd7578ef32bae80e6689d9177f
Haley Jacobs <haleyljacobs@gmail.com>
2019-03-06 10:14:00 -0700

    Added styling to Page.js component

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
+import styles from 'styled-components';
+
+const MyButton = styled.button`
+	background: red;
+	font-size: ${props => (props.huge ? '100px' : '50px')};
+	.car {
+		font: 100px;
+	}
+`;
 
 class Page extends Component {
 	render() {
 		return (
 			<div>
-				<p>Hey I'm the page Component</p>
+				<Meta />
+				<Header />
+				<MyButton>
+					Click Me
+					<span className='car'>car</span>
+				</MyButton>
 				{this.props.children}
 			</div>
 		)
